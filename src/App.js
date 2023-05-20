b1c118d2a85c086ec0afb0217dea371e6c133b80
Abdullah Khan <abdullahkhan.sse@outlook.com>
2023-05-20 20:30:59 +0530

    Use Context Hook

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
-import React from 'react';
-import Button from './Button';
+import React, { createContext, useContext, useState } from 'react';
 
-function App() {
-  const handleClick = () => {
-    console.log('Button clicked!');
+// Create a context object
+const ThemeContext = createContext();
+
+const App = () => {
+  const [theme, setTheme] = useState('light');
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
   };
 
   return (
+    // Provide the shared data through the Provider
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <div>
+        <h1>App</h1>
+        <Toolbar />
+      </div>
+    </ThemeContext.Provider>
+  );
+};
+
+const Toolbar = () => {
+  // Consume the shared data using the useContext hook
+  const { theme, toggleTheme } = useContext(ThemeContext);
+
+  return (
     <div>
-      <Button onClick={handleClick} label="Click me" />
+      <button onClick={toggleTheme}>Toggle Theme</button>
+      <div>Current Theme: {theme}</div>
     </div>
   );
-}
+};
 
 export default App;
